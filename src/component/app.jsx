a8c5fa8829dbc155b634dbf99d6a19cf6f77292a
marc1 <mvrcys@gmail.com>
2020-10-08 18:07:07 -0700

    Cleaned up old ContentFrame structure and related routes.

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import * as routes from 'src/const/routes.js' 
 
 import Landing from 'src/route/landing/landing.jsx'
-import About from 'src/route/about/about.jsx'
-import Contact from 'src/route/contact/contact.jsx'
 
 class App extends React.Component {
     render() {
@@ -13,8 +11,6 @@ class App extends React.Component {
             <Router>
                 <Switch>
                     <Route exact path={routes.ROUTE_LANDING} component={ Landing }/>
-                    <Route path={routes.ROUTE_ABOUT} component={ About }/>
-                    <Route path={routes.ROUTE_CONTACT} component={ Contact }/>
                 </Switch>
             </Router>
         )
--- src/const/routes.js
+++ src/const/routes.js
@@ -5,7 +5,5 @@ export const GITHUB = 'https://www.github.com/marc1/';
 
 // Routes to be rendered by Navigation components
 export const ROUTES = [
-    { label: "ABOUT", url: ROUTE_ABOUT},
-    { label: "CONTACT", url: ROUTE_CONTACT},
     { label: "GITHUB", url: GITHUB, external: true }
 ];
